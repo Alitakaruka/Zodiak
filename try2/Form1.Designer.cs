1cf5d61359b4e3056b18c3de510d0f45cd94819b
Alitakaruka <galashevich69@mail.ru>
2020-11-15 04:44:22 +0300

    Добавьте файлы проекта.

---
+++ try2/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace try2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(195, 7);
+            this.maskedTextBox1.Mask = "00/00/0000";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(274, 22);
+            this.maskedTextBox1.TabIndex = 0;
+            this.maskedTextBox1.ValidatingType = typeof(System.DateTime);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(166, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите дату рождения";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(513, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Вывести";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(179, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(-3, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(156, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Ваш возраст (в годах:)";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(215, 175);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 17);
+            this.label4.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(-3, 175);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(166, 17);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "День вашего рождения:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(-3, 266);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(102, 17);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Знак зодиака:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(113, 266);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 17);
+            this.label7.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ try2/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace try2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+      
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DateTime d;
+            d = DateTime.Today;
+            DateTime x = Convert.ToDateTime(maskedTextBox1.Text);
+  
+            DateTime  date = Convert.ToDateTime (maskedTextBox1.Text);
+            int years = d.Year - date.Year;
+            label2.Text = Convert.ToString(years);
+
+            label4.Text =Convert.ToString (date.DayOfWeek);
+
+            switch (label4.Text) {
+                case "Monday":
+                    label4.Text += "(Понедельник)";
+                    break;
+                case "Tuesday":
+                    label4.Text += "(Вторник)";
+                    break;
+                case "Wednesday":
+                    label4.Text += "(Среда)";
+                    break;
+                case "Thursday":
+                    label4.Text += "(Четверг)";
+                    break;
+                case "Friday":
+                    label4.Text += "(Пятница)";
+                    break;
+                case "Saturday":
+                    label4.Text += "(Суббота)";
+                    break;
+                case "Sunday":
+                    label4.Text += "(Высплюсенье)";
+                        break;
+                    default:
+                    break;
+            }
+
+            DateTime vodolei = new DateTime(date.Year, 01, 20);   
+            DateTime vodolei2 = new DateTime(date.Year, 02, 18);
+            
+            DateTime riba = new DateTime(date.Year, 02, 19);   
+            DateTime riba2 = new DateTime(date.Year, 03, 20);   
+
+            DateTime oven = new DateTime(date.Year, 03, 21);   
+            DateTime oven2 = new DateTime(date.Year, 04, 19);
+
+            DateTime telec = new DateTime(date.Year, 04, 20);
+            DateTime telec2 = new DateTime(date.Year, 05, 20);
+
+            DateTime twins = new DateTime(date.Year, 05, 21);
+            DateTime twin2s = new DateTime(date.Year, 06, 21);
+
+            DateTime rak = new DateTime(date.Year, 06, 22);
+            DateTime rak2 = new DateTime(date.Year, 07, 22);
+
+            DateTime lev = new DateTime(date.Year, 07, 23);
+            DateTime lev2 = new DateTime(date.Year, 08, 22);
+
+            DateTime deva = new DateTime(date.Year, 08, 23);
+            DateTime deva2 = new DateTime(date.Year, 09, 22);
+
+            DateTime vesi = new DateTime(date.Year, 09, 23);
+            DateTime vesi2 = new DateTime(date.Year, 10, 22); 
+
+            DateTime skorpion = new DateTime(date.Year, 10, 23);
+            DateTime skorpion2 = new DateTime(date.Year, 11, 22);
+
+            DateTime strelec = new DateTime(date.Year, 11, 23);
+            DateTime strelec2 = new DateTime(date.Year, 12, 21);
+
+            DateTime kozerog = new DateTime(date.Year, 11, 22);
+            DateTime kozerog2 = new DateTime(date.Year-1, 01, 19);
+
+            if (date == vodolei || date <= vodolei2)
+            {
+                label7.Text = "Водолей";
+            }
+            if (date == riba || date <= riba2)
+            {
+                label7.Text = "рыба";
+            }
+            if (date == oven || date <= oven2)
+            {
+                label7.Text = "Овен";
+            }
+            if (date == telec || date <= telec2)
+            {
+                label7.Text = "Телец";
+            }
+            if (date == twins || date <= twin2s)
+            {
+                label7.Text = "Близнецы";
+            }
+            if (date == rak || date <= rak2)
+            {
+                label7.Text = "Рак";
+            }
+            if (date == lev || date <= lev2)
+            {
+                label7.Text = "Лев";
+            }
+            if (date == deva || date <= deva2)
+            {
+                label7.Text = "Дева";
+            }
+            if (date == vesi || date <= vesi2)
+            {
+                label7.Text = "Весы";
+            }
+            if (date == skorpion || date <= skorpion2)
+            {
+                label7.Text = "Скорпион";
+            }
+            if (date == strelec || date <= strelec2)
+            {
+                label7.Text = "Стрелец";
+            }
+            if (date == kozerog || date <= kozerog2)
+            {
+                label7.Text = "Козерог";
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+        
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
